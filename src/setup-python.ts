04b88679183d4f500054e12d8bc671140dae29e8
Danny McCormick <damccorm@microsoft.com>
2019-07-03 14:04:51 -0400

    Fail on error

--- src/setup-python.ts
+++ src/setup-python.ts
@@ -2,10 +2,14 @@ import * as core from '@actions/core';
 import * as finder from './find-python';
 
 async function run() {
-  const version: string = core.getInput('version');
-  if (version) {
-    const arch: string = core.getInput('architecture', {required: true});
-    await finder.findPythonVersion(version, arch);
+  try {
+    const version: string = core.getInput('version');
+    if (version) {
+      const arch: string = core.getInput('architecture', {required: true});
+      await finder.findPythonVersion(version, arch);
+    }
+  } catch (err) {
+    core.setFailed(err.message);
   }
 }
 
