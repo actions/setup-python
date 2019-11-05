08bf913604480fd6ea3b7b8a160c4ad0ec1b439e
Konrad Pabjan <Konrad.Pabjan@microsoft.com>
2019-11-05 17:42:30 -0500

    Update release script

--- dist/index.js
+++ dist/index.js
@@ -34,7 +34,7 @@ module.exports =
 /******/ 	// the startup function
 /******/ 	function startup() {
 /******/ 		// Load entry module and return exports
-/******/ 		return __webpack_require__(264);
+/******/ 		return __webpack_require__(429);
 /******/ 	};
 /******/
 /******/ 	// run startup
@@ -71,7 +71,21 @@ module.exports = require("https");
 
 /***/ }),
 
-/***/ 264:
+/***/ 357:
+/***/ (function(module) {
+
+module.exports = require("assert");
+
+/***/ }),
+
+/***/ 417:
+/***/ (function(module) {
+
+module.exports = require("crypto");
+
+/***/ }),
+
+/***/ 429:
 /***/ (function(module, __unusedexports, __webpack_require__) {
 
 module.exports =
@@ -144,6 +158,82 @@ module.exports = __webpack_require__(129);
 /***/ (function(module) {
 
 module.exports = __webpack_require__(211);
+
+/***/ }),
+
+/***/ 264:
+/***/ (function(module, __unusedexports, __nested_webpack_require_1759__) {
+
+module.exports =
+/******/ (function(modules, runtime) { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	__webpack_require__.ab = __dirname + "/";
+/******/
+/******/ 	// the startup function
+/******/ 	function startup() {
+/******/ 		// Load entry module and return exports
+/******/ 		return __webpack_require__(264);
+/******/ 	};
+/******/
+/******/ 	// run startup
+/******/ 	return startup();
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ 16:
+/***/ (function(module) {
+
+module.exports = __nested_webpack_require_1759__(16);
+
+/***/ }),
+
+/***/ 87:
+/***/ (function(module) {
+
+module.exports = __nested_webpack_require_1759__(87);
+
+/***/ }),
+
+/***/ 129:
+/***/ (function(module) {
+
+module.exports = __nested_webpack_require_1759__(129);
+
+/***/ }),
+
+/***/ 211:
+/***/ (function(module) {
+
+module.exports = __nested_webpack_require_1759__(211);
 
 /***/ }),
 
@@ -5247,6 +5337,73 @@ module.exports = __nested_webpack_require_1759__(835);
 /***/ 357:
 /***/ (function(module) {
 
+module.exports = __nested_webpack_require_1759__(357);
+
+/***/ }),
+
+/***/ 417:
+/***/ (function(module) {
+
+module.exports = __nested_webpack_require_1759__(417);
+
+/***/ }),
+
+/***/ 605:
+/***/ (function(module) {
+
+module.exports = __nested_webpack_require_1759__(605);
+
+/***/ }),
+
+/***/ 614:
+/***/ (function(module) {
+
+module.exports = __nested_webpack_require_1759__(614);
+
+/***/ }),
+
+/***/ 622:
+/***/ (function(module) {
+
+module.exports = __nested_webpack_require_1759__(622);
+
+/***/ }),
+
+/***/ 631:
+/***/ (function(module) {
+
+module.exports = __nested_webpack_require_1759__(631);
+
+/***/ }),
+
+/***/ 669:
+/***/ (function(module) {
+
+module.exports = __nested_webpack_require_1759__(669);
+
+/***/ }),
+
+/***/ 747:
+/***/ (function(module) {
+
+module.exports = __nested_webpack_require_1759__(747);
+
+/***/ }),
+
+/***/ 835:
+/***/ (function(module) {
+
+module.exports = __nested_webpack_require_1759__(835);
+
+/***/ })
+
+/******/ });
+
+/***/ }),
+
+/***/ 357:
+/***/ (function(module) {
+
 module.exports = __webpack_require__(357);
 
 /***/ }),
@@ -5308,20 +5465,6 @@ module.exports = __webpack_require__(835);
 /***/ })
 
 /******/ });
-
-/***/ }),
-
-/***/ 357:
-/***/ (function(module) {
-
-module.exports = require("assert");
-
-/***/ }),
-
-/***/ 417:
-/***/ (function(module) {
-
-module.exports = require("crypto");
 
 /***/ }),
 
