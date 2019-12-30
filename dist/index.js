350318022136c903b1dcf90854e65700f78938e1
Konrad Pabjan <kopabjan@microsoft.com>
2019-12-30 16:34:28 -0500

    Update dist/index.js

--- dist/index.js
+++ dist/index.js
@@ -34,7 +34,7 @@ module.exports =
 /******/ 	// the startup function
 /******/ 	function startup() {
 /******/ 		// Load entry module and return exports
-/******/ 		return __webpack_require__(264);
+/******/ 		return __webpack_require__(429);
 /******/ 	};
 /******/
 /******/ 	// run startup
@@ -71,8 +71,98 @@ module.exports = require("https");
 
 /***/ }),
 
-/***/ 264:
+/***/ 357:
+/***/ (function(module) {
+
+module.exports = require("assert");
+
+/***/ }),
+
+/***/ 417:
+/***/ (function(module) {
+
+module.exports = require("crypto");
+
+/***/ }),
+
+/***/ 429:
 /***/ (function(module, __unusedexports, __webpack_require__) {
+
+module.exports =
+/******/ (function(modules, runtime) { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	__webpack_require__.ab = __dirname + "/";
+/******/
+/******/ 	// the startup function
+/******/ 	function startup() {
+/******/ 		// Load entry module and return exports
+/******/ 		return __webpack_require__(264);
+/******/ 	};
+/******/
+/******/ 	// run startup
+/******/ 	return startup();
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ 16:
+/***/ (function(module) {
+
+module.exports = __webpack_require__(16);
+
+/***/ }),
+
+/***/ 87:
+/***/ (function(module) {
+
+module.exports = __webpack_require__(87);
+
+/***/ }),
+
+/***/ 129:
+/***/ (function(module) {
+
+module.exports = __webpack_require__(129);
+
+/***/ }),
+
+/***/ 211:
+/***/ (function(module) {
+
+module.exports = __webpack_require__(211);
+
+/***/ }),
+
+/***/ 264:
+/***/ (function(module, __unusedexports, __nested_webpack_require_1833__) {
 
 module.exports =
 /******/ (function(modules, runtime) { // webpackBootstrap
@@ -122,42 +212,42 @@ module.exports =
 /***/ 16:
 /***/ (function(module) {
 
-module.exports = __webpack_require__(16);
+module.exports = __nested_webpack_require_1833__(16);
 
 /***/ }),
 
 /***/ 87:
 /***/ (function(module) {
 
-module.exports = __webpack_require__(87);
+module.exports = __nested_webpack_require_1833__(87);
 
 /***/ }),
 
 /***/ 129:
 /***/ (function(module) {
 
-module.exports = __webpack_require__(129);
+module.exports = __nested_webpack_require_1833__(129);
 
 /***/ }),
 
 /***/ 211:
 /***/ (function(module) {
 
-module.exports = __webpack_require__(211);
+module.exports = __nested_webpack_require_1833__(211);
 
 /***/ }),
 
 /***/ 357:
 /***/ (function(module) {
 
-module.exports = __webpack_require__(357);
+module.exports = __nested_webpack_require_1833__(357);
 
 /***/ }),
 
 /***/ 417:
 /***/ (function(module) {
 
-module.exports = __webpack_require__(417);
+module.exports = __nested_webpack_require_1833__(417);
 
 /***/ }),
 
@@ -5547,6 +5637,73 @@ module.exports = __nested_webpack_require_2019__(835);
 /***/ 605:
 /***/ (function(module) {
 
+module.exports = __nested_webpack_require_1833__(605);
+
+/***/ }),
+
+/***/ 614:
+/***/ (function(module) {
+
+module.exports = __nested_webpack_require_1833__(614);
+
+/***/ }),
+
+/***/ 622:
+/***/ (function(module) {
+
+module.exports = __nested_webpack_require_1833__(622);
+
+/***/ }),
+
+/***/ 631:
+/***/ (function(module) {
+
+module.exports = __nested_webpack_require_1833__(631);
+
+/***/ }),
+
+/***/ 669:
+/***/ (function(module) {
+
+module.exports = __nested_webpack_require_1833__(669);
+
+/***/ }),
+
+/***/ 747:
+/***/ (function(module) {
+
+module.exports = __nested_webpack_require_1833__(747);
+
+/***/ }),
+
+/***/ 835:
+/***/ (function(module) {
+
+module.exports = __nested_webpack_require_1833__(835);
+
+/***/ })
+
+/******/ });
+
+/***/ }),
+
+/***/ 357:
+/***/ (function(module) {
+
+module.exports = __webpack_require__(357);
+
+/***/ }),
+
+/***/ 417:
+/***/ (function(module) {
+
+module.exports = __webpack_require__(417);
+
+/***/ }),
+
+/***/ 605:
+/***/ (function(module) {
+
 module.exports = __webpack_require__(605);
 
 /***/ }),
@@ -5594,20 +5751,6 @@ module.exports = __webpack_require__(835);
 /***/ })
 
 /******/ });
-
-/***/ }),
-
-/***/ 357:
-/***/ (function(module) {
-
-module.exports = require("assert");
-
-/***/ }),
-
-/***/ 417:
-/***/ (function(module) {
-
-module.exports = require("crypto");
 
 /***/ }),
 
