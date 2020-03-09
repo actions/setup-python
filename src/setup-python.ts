8ad00a65a6cf71b71cdf47d3e77dbb16669d1086
Konrad Pabjan <konradpabjan@github.com>
2020-03-09 10:16:37 +0100

    Updates to npm packages (#66)
    * npm package updates
    
    * Updates to ncc build

--- src/setup-python.ts
+++ src/setup-python.ts
@@ -8,12 +8,10 @@ async function run() {
     if (version) {
       const arch: string = core.getInput('architecture', {required: true});
       const installed = await finder.findPythonVersion(version, arch);
-      console.log(
-        `Successfully setup ${installed.impl} (${installed.version}).`
-      );
+      core.info(`Successfully setup ${installed.impl} (${installed.version})`);
     }
     const matchersPath = path.join(__dirname, '..', '.github');
-    console.log(`##[add-matcher]${path.join(matchersPath, 'python.json')}`);
+    core.info(`##[add-matcher]${path.join(matchersPath, 'python.json')}`);
   } catch (err) {
     core.setFailed(err.message);
   }
