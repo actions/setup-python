e6ed85bb3a689cecfae9bb3e6ad4e27475bc6d82
Shubham Tiwari <tiwarishub@github.com>
2022-03-29 14:38:25 +0530

    format check

--- src/setup-python.ts
+++ src/setup-python.ts
@@ -14,9 +14,13 @@ function isPyPyVersion(versionSpec: string) {
 async function cacheDependencies(cache: string, pythonVersion: string) {
   if (!actionsCache.isFeatureAvailable()) {
     if (isGhes()) {
-      throw new Error('Caching is only supported on GHES version >= 3.5. If you are on version >=3.5 Please check with GHES admin if Actions cache service is enabled or not.');
+      throw new Error(
+        'Caching is only supported on GHES version >= 3.5. If you are on version >=3.5 Please check with GHES admin if Actions cache service is enabled or not.'
+      );
     } else {
-      throw new Error('An internal error has occurred in cache backend. Please check https://www.githubstatus.com/ for any ongoing issue in actions.');
+      throw new Error(
+        'An internal error has occurred in cache backend. Please check https://www.githubstatus.com/ for any ongoing issue in actions.'
+      );
     }
   }
   const cacheDependencyPath =
