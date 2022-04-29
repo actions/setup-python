22daa094b838330a3ab8897dbf26828efd05d952
Sergey Dolin <sergey.dolin@gmail.com>
2022-04-29 09:29:36 +0500

    Add generated files

--- dist/setup/index.js
+++ dist/setup/index.js
@@ -6112,6 +6112,9 @@ function run() {
                     yield cacheDependencies(cache, pythonVersion);
                 }
             }
+            else {
+                core.warning('python-version is empty, the OS native python will be used');
+            }
             const matchersPath = path.join(__dirname, '../..', '.github');
             core.info(`##[add-matcher]${path.join(matchersPath, 'python.json')}`);
         }
