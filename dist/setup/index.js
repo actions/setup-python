1d95b404f29d0d8392fc45892c17fd211482429a
MaksimZhukov <maksimzhukov@github.com>
2022-12-06 10:23:32 +0100

    Regenerate the dist folder

--- dist/setup/index.js
+++ dist/setup/index.js
@@ -66379,7 +66379,7 @@ function useCpythonVersion(version, architecture, updateEnvironment, checkLatest
         if (!installDir) {
             const osInfo = yield utils_1.getOSInfo();
             throw new Error([
-                `Version ${version} with arch ${architecture} not found for ${osInfo}`,
+                `The version '${version}' with architecture '${architecture}' was not found for ${osInfo ? osInfo : 'this operating system'}.`,
                 `The list of all available versions can be found here: ${installer.MANIFEST_URL}`
             ].join(os.EOL));
         }
