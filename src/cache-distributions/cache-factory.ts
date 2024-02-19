d9e42e5be0ab0c200805c65d3b2edeef6245da1c
Aarni Koskela <akx@iki.fi>
2024-02-19 13:30:09 +0200

    Add support for caching `uv`
    See https://github.com/astral-sh/uv

--- src/cache-distributions/cache-factory.ts
+++ src/cache-distributions/cache-factory.ts
@@ -1,11 +1,13 @@
 import PipCache from './pip-cache';
 import PipenvCache from './pipenv-cache';
 import PoetryCache from './poetry-cache';
+import UvCache from './uv-cache';
 
 export enum PackageManagers {
   Pip = 'pip',
   Pipenv = 'pipenv',
-  Poetry = 'poetry'
+  Poetry = 'poetry',
+  Uv = 'uv'
 }
 
 export function getCacheDistributor(
@@ -20,6 +22,8 @@ export function getCacheDistributor(
       return new PipenvCache(pythonVersion, cacheDependencyPath);
     case PackageManagers.Poetry:
       return new PoetryCache(pythonVersion, cacheDependencyPath);
+    case PackageManagers.Uv:
+      return new UvCache(pythonVersion, cacheDependencyPath);
     default:
       throw new Error(`Caching for '${packageManager}' is not supported`);
   }
