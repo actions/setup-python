777921ea78e1a05939fbffc01f6197ff677cfab6
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2025-05-17 19:57:21 +0200

    Include python version in PyPy python-version output

--- dist/setup/index.js
+++ dist/setup/index.js
@@ -95953,7 +95953,7 @@ function findPyPyVersion(versionSpec, architecture, updateEnvironment, checkLate
             core.addPath(pythonLocation);
             core.addPath(_binDir);
         }
-        core.setOutput('python-version', 'pypy' + resolvedPyPyVersion);
+        core.setOutput('python-version', `pypy${resolvedPythonVersion}-${resolvedPyPyVersion}`);
         core.setOutput('python-path', pythonPath);
         return { resolvedPyPyVersion, resolvedPythonVersion };
     });
